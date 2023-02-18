10000cf2f8505b92a885bc6f8cb9b1fd7148cc53
Charles Brendon <charlesbrendonss@gmail.com>
2023-02-17 21:06:22 -0300

    instalando validação usando zod

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -5,13 +5,14 @@ const {
   updateUser,
   removeUser,
 } = require("../repositories/user");
-const bcrypt = require("bcrypt")
-
+const bcrypt = require("bcrypt");
+const { userValidation } = require("../validations/user");
 
 exports.create = async (req, res) => {
   try {
-    req.body.password = bcrypt.hashSync(req.body.password, 10)
-    const user = await createUser(req.body);
+    const data = await userValidation.parse(req.body)
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    const user = await createUser(data);
     res.status(200).send(user);
   } catch (e) {
     res.status(400).send(e);
@@ -19,39 +20,37 @@ exports.create = async (req, res) => {
 };
 
 exports.getId = async (req, res) => {
-    try {
-      const user = await getById(Number(req.params.id));
-      res.status(200).send(user);
-    } catch (e) {
-      res.status(400).send(e);
-    }
-  };
+  try {
+    const user = await getById(Number(req.params.id));
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
 
-  exports.get = async (req, res) => {
-    try {
-      const users = await getUsers();
-      res.status(200).send(users);
-    } catch (e) {
-      res.status(400).send(e);
-    }
-  };
+exports.get = async (req, res) => {
+  try {
+    const users = await getUsers();
+    res.status(200).send(users);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
 
-  exports.update = async (req, res) => {
-    try {
-      const user = await updateUser(Number(req.params.id), req.body);
-      res.status(200).send(user);
-    } catch (e) {
-      res.status(400).send(e);
-    }
-  };
+exports.update = async (req, res) => {
+  try {
+    const user = await updateUser(Number(req.params.id), req.body);
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
 
-  exports.remove = async (req, res) => {
-    try {
-      await removeUser(Number(req.params.id));
-      res.status(200).send();
-    } catch (e) {
-      res.status(400).send(e);
-    }
-  };
-
-  
+exports.remove = async (req, res) => {
+  try {
+    await removeUser(Number(req.params.id));
+    res.status(200).send();
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
---
+++ src/validations/user.js
@@ -0,0 +1,8 @@
+const { z } = require("zod")
+
+exports.userValidation = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string()
+
+})
